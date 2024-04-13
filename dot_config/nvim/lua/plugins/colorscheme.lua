e7dbc9266399be7f973322adbe005fb8c564106b
Yurb <yavurb@gmail.com>
2024-04-13 08:36:19 -0400

    :tada: initial commit

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priorioty = 1000 },
+  { "sainnhe/gruvbox-material" },
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/sonokai" },
+  { "sainnhe/everforest" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
