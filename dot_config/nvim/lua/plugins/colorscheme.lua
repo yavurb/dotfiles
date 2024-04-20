1ef5c5b466651efddb090b8264d6ef485fadc0bf
Yurb <yavurb@gmail.com>
2024-04-20 00:10:56 -0400

    :recycle: refactor: use gruvbox-material color scheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priorioty = 1000 },
-  { "sainnhe/gruvbox-material" },
+  {
+    "sainnhe/gruvbox-material",
+  },
   { "rebelot/kanagawa.nvim" },
-  { "sainnhe/sonokai" },
-  { "sainnhe/everforest" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "gruvbox-material",
     },
   },
 }
