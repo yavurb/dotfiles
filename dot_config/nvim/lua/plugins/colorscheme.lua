e15c4c597d2ab2c39274a9f52a3e723367692f2a
Yurb <yavurb@gmail.com>
2024-08-11 15:43:04 -0400

    :wrench: refactor: change tmux and bat themes

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,59 +1,65 @@
--- -- Gruvbox Material
--- vim.g.gruvbox_material_background = "hard"
--- vim.api.nvim_create_autocmd("ColorScheme", {
---   group = vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {}),
---   pattern = "gruvbox-material",
---   callback = function()
---     local config = vim.fn["gruvbox_material#get_configuration"]()
---     local palette = vim.fn["gruvbox_material#get_palette"](config.background, config.foreground, config.colors_override)
---     local set_hl = vim.fn["gruvbox_material#highlight"]
---
---     -- set_hl("TelescopeNormal", palette.none, palette.bg_dim)
---     -- set_hl("TelescopeBorder", palette.bg_dim, palette.bg_dim)
---     set_hl("TelescopeTitle", palette.yellow, palette.none, "bold")
---     set_hl("TelescopePromptNormal", palette.none, palette.bg3)
---     set_hl("TelescopePromptBorder", palette.bg3, palette.bg3)
---     set_hl("TelescopeResultsNormal", palette.fg0, palette.bg1)
---     set_hl("TelescopeResultsBorder", palette.bg1, palette.bg1)
---     set_hl("TelescopePreviewNormal", palette.none, palette.bg_dim)
---     set_hl("TelescopePreviewBorder", palette.bg_dim, palette.bg_dim)
---   end,
--- })
-
--- Everforest
--- vim.g.everforest_enable_italic = true
--- vim.g.everforest_background = "hard"
-
 return {
   {
-    "sainnhe/gruvbox-material",
+    "sainnhe/everforest",
+    enable = false,
+    config = function()
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_enable_italic = true
+    end,
   },
-  -- {
-  --   "EdenEast/nightfox.nvim",
-  --   opts = {
-  --     options = {
-  --       styles = {
-  --         comments = "italic",
-  --         keywords = "bold",
-  --         functions = "italic",
-  --         variables = "italic",
-  --       },
-  --     },
-  --     groups = {
-  --       all = {
-  --         -- TelescopeTitle = { fg = "palette.sel1", style = "bold" },
-  --         TelescopePromptNormal = { bg = "palette.bg3" },
-  --         TelescopePromptBorder = { fg = "palette.bg3", bg = "palette.bg3" },
-  --         TelescopeResultsNormal = { fg = "palette.fg0", bg = "palette.bg2" },
-  --         TelescopeResultsBorder = { fg = "palette.bg2", bg = "palette.bg2" },
-  --         TelescopePreviewNormal = { bg = "palette.bg0" },
-  --         TelescopePreviewBorder = { bg = "palette.bg0", fg = "palette.bg0" },
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "sainnhe/gruvbox-material",
+    enable = false,
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {}),
+        pattern = "gruvbox-material",
+        callback = function()
+          local config = vim.fn["gruvbox_material#get_configuration"]()
+          local palette =
+            vim.fn["gruvbox_material#get_palette"](config.background, config.foreground, config.colors_override)
+          local set_hl = vim.fn["gruvbox_material#highlight"]
+
+          set_hl("TelescopeTitle", palette.yellow, palette.none, "bold")
+          set_hl("TelescopePromptNormal", palette.none, palette.bg3)
+          set_hl("TelescopePromptBorder", palette.bg3, palette.bg3)
+          set_hl("TelescopeResultsNormal", palette.fg0, palette.bg1)
+          set_hl("TelescopeResultsBorder", palette.bg1, palette.bg1)
+          set_hl("TelescopePreviewNormal", palette.none, palette.bg_dim)
+          set_hl("TelescopePreviewBorder", palette.bg_dim, palette.bg_dim)
+        end,
+      })
+    end,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    enable = false,
+    opts = {
+      options = {
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          functions = "italic",
+          variables = "italic",
+        },
+      },
+      groups = {
+        all = {
+          -- TelescopeTitle = { fg = "palette.sel1", style = "bold" },
+          TelescopePromptNormal = { bg = "palette.bg3" },
+          TelescopePromptBorder = { fg = "palette.bg3", bg = "palette.bg3" },
+          TelescopeResultsNormal = { fg = "palette.fg0", bg = "palette.bg2" },
+          TelescopeResultsBorder = { fg = "palette.bg2", bg = "palette.bg2" },
+          TelescopePreviewNormal = { bg = "palette.bg0" },
+          TelescopePreviewBorder = { bg = "palette.bg0", fg = "palette.bg0" },
+        },
+      },
+    },
+  },
   {
     "rebelot/kanagawa.nvim",
+    enable = false,
     opts = {
       commentStyle = { italic = true },
       overrides = function(colors)
@@ -70,7 +76,6 @@ return {
       end,
     },
   },
-  -- { "sainnhe/everforest" },
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -83,6 +88,13 @@ return {
           crust = "#0e0e0e",
         },
       },
+      highlight_overrides = {
+        mocha = function(_)
+          return {
+            LspInlayHint = { bg = "bg" },
+          }
+        end,
+      },
       integrations = {
         telescope = {
           enable = true,
@@ -94,9 +106,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "gruvbox-material",
       colorscheme = "catppuccin",
-      -- colorscheme = "nightfox",
     },
   },
 }
