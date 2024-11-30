7f41eede4c849f778f8a5ab7d4ee1d0194fbb1bd
roīïī <yavurb@gmail.com>
2024-11-30 08:56:08 -0400

    :sparkles: refactor(nvim): update deps and modify snacks dashboard

---
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  -- {
+  --   "saghen/blink.cmp",
+  --   ---@module 'blink.cmp'
+  --   ---@type blink.cmp.Config
+  --   opts = {
+  --     nerd_font_variant = "normal",
+  --
+  --     trigger = { signature_help = { enabled = true } },
+  --   },
+  -- },
+}
--- dot_config/nvim/lua/plugins/pkl.lua
+++ dot_config/nvim/lua/plugins/pkl.lua
@@ -9,16 +9,11 @@ return {
         vim.cmd("TSUpdate")
       end,
     },
-    "L3MON4D3/LuaSnip",
   },
   build = function()
     require("pkl-neovim.internal").init()
 
     -- Set up syntax highlighting.
     vim.cmd("TSInstall! pkl")
-  end,
-  config = function()
-    -- Set up snippets.
-    require("luasnip.loaders.from_snipmate").lazy_load()
   end,
 }
